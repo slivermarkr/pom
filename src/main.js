10456d1b6b8b783fbda02aea444f6843f419f0cd
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-12-31 18:15:06 +0800

    sesh end

--- src/main.js
+++ src/main.js
@@ -7,17 +7,30 @@ const selection = document.querySelector("#selection");
 
 const pom = new Pomodoro();
 
+const defaultTime = { time: 25, short: 5, long: 10 };
+let timeInMilliSeconds = pom.getTimeData(defaultTime);
+let dataAfterPause = undefined;
+
 const state = {
   isPaused: false,
+  activeTimer: timeInMilliSeconds.time,
 };
-
-const defaultTime = { time: 25, short: 5, long: 10 };
-let dataAfterPause = undefined;
 
 function pad(string) {
   return String(string).padStart(2, 0);
 }
 
+function updateDisplay(data) {
+  timeContainer.textContent = "";
+  timeContainer.textContent = `${pad(data.min)}:${pad(data.sec)}`;
+
+  dataAfterPause = data;
+}
+
+function initDisplay(min, sec = "0") {
+  timeContainer.textContent = "";
+  timeContainer.textContent = `${pad(min)}:${pad(sec)}`;
+}
 function createBtn(type) {
   const btn = document.createElement("button");
   if (type === "Start") {
@@ -29,26 +42,52 @@ function createBtn(type) {
   }
   return btn;
 }
+
+selection.addEventListener("change", (e) => {
+  switch (selection.value) {
+    case "Pomodoro":
+      state.activeTimer = timeInMilliSeconds.time;
+      dataAfterPause = undefined;
+      pom.pauseTimer();
+      initDisplay(defaultTime.time);
+      break;
+
+    case "Short Break":
+      state.activeTimer = timeInMilliSeconds.short;
+      console.log(state.activeTimer);
+      initDisplay(defaultTime.short);
+      dataAfterPause = undefined;
+      pom.pauseTimer();
+      break;
+
+    case "Long Break":
+      state.activeTimer = timeInMilliSeconds.long;
+      initDisplay(defaultTime.long);
+      dataAfterPause = undefined;
+      pom.pauseTimer();
+      break;
+    default:
+      break;
+  }
+});
+
+function runTheTimer(timer) {
+  pom.initTimer(timer, updateDisplay);
+}
+
 container.addEventListener("click", (e) => {
   if (!e.target.id.includes("startBtn")) return;
   const stopBtn = createBtn("Stop");
   selection.insertAdjacentElement("beforebegin", stopBtn);
 
-  let timeInMilliSeconds = pom.getTimeData(defaultTime);
-
   if (state.isPaused) {
-    timeInMilliSeconds.time = dataAfterPause.timeInMilli;
+    if (dataAfterPause) {
+      timeInMilliSeconds.time = dataAfterPause.timeInMilli;
+    }
+    runTheTimer(state.activeTimer);
     state.isPaused = false;
-    console.log(timeInMilliSeconds);
-  }
-
-  {
-    pom.initTimer(timeInMilliSeconds.time, function updateDisplay(data) {
-      timeContainer.textContent = "";
-      timeContainer.textContent = `${pad(data.min)}:${pad(data.sec)}`;
-
-      dataAfterPause = data;
-    });
+  } else {
+    runTheTimer(state.activeTimer);
   }
 
   e.target.remove();
@@ -65,23 +104,3 @@ container.addEventListener("click", (e) => {
   selection.insertAdjacentElement("beforebegin", startBtn);
   e.target.remove();
 });
-
-//const millis = pom.getTimeData({ time: 25, short: 5, long: 10 });
-//let pausedData = undefined;
-//
-//pom.initTimer(millis.time, function (data) {
-//  console.log(data);
-//  pausedData = data;
-//});
-//
-//setTimeout(function () {
-//  pom.pauseTimer();
-//  console.log("This is the paused data", pausedData);
-//  setTimeout(function () {
-//    pom.initTimer(pausedData.timeInMilli, function (data) {
-//      console.log(data);
-//      pausedData = data;
-//    });
-//  }, 3000);
-//}, 3000);
-console.log("Hello");
